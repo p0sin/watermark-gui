f50316a3392fbab8459040edaa5913529f319afe
p0sin <elposin@gmail.com>
2024-03-18 16:51:34 -0600

    add img canvas and edit menu layout

--- main.py
+++ main.py
@@ -10,15 +10,56 @@ class App(ctk.CTk):
 
         # window setup
         self.title('Watermark GUI')
-        self.geometry('800x600')
-        self.minsize(800, 600)
+        self.geometry('1000x600')
+        self.minsize(1000, 600)
 
         self.rowconfigure(0, weight=1)
         self.columnconfigure(0, weight=1)
-        self.columnconfigure(1, weight=3)
+        self.columnconfigure(1, weight=6)
 
-        AddFile(self)
-
+        # Create an instance of AddFile with the callback function
+        self.add_file = AddFile(self, self.import_image)
+        
         self.mainloop()
+
+    def import_image(self, path):
+        self.image = Image.open(path)
+        self.image_ratio = self.image.size[0] / self.image.size[1]
+        self.image_tk = ImageTk.PhotoImage(self.image)
+        
+        self.add_file.grid_forget()
+        self.image_output = ImageCanvas(self, self.resize_image)
+        self.edit_menu = EditMenu(self, self.close_edit)
+
+
+    def close_edit(self):
+        self.image_output.grid_forget()
+        self.edit_menu.grid_forget()
+
+        self.add_file = AddFile(self, self.import_image)
+
+    def resize_image(self, event):
+
+        # current canvas ratio
+        canvas_ratio = event.width / event.height 
+
+        # resize
+        if canvas_ratio > self.image_ratio: # canvas is wider than then image
+            image_height = int(event.height)
+            image_width = int(image_height * self.image_ratio)
+        else: # canvas is taller than image
+            image_width = int(event.width)
+            image_height = int(image_width / self.image_ratio)
+
+
+        # place image
+        self.image_output.delete('all')
+        resized_image = self.image.resize((image_width, image_height))
+        self.image_tk = ImageTk.PhotoImage(resized_image)
+        self.image_output.create_image(event.width / 2, event.height / 2, image=self.image_tk)
+        
+
+    
+    
 
 App()
--- widgets.py
+++ widgets.py
@@ -3,8 +3,10 @@ from PIL import Image, ImageTk
 from tkinter import filedialog, Canvas
 
 class AddFile(ctk.CTkFrame):
-    def __init__(self, parent):
+    def __init__(self, parent, import_func):
         super().__init__(parent)
+
+        self.import_func = import_func
 
         # widgets
         self.label = ctk.CTkLabel(self, text='Add Watermark', font=('Arial', 20))
@@ -15,8 +17,69 @@ class AddFile(ctk.CTkFrame):
         self.label.place(relx=0.5, rely=0.45, anchor='center')
         self.button.place(relx=0.5, rely=0.5, anchor='center')
 
+
     def open_image(self):
-        path = filedialog.askopenfile().name
+        path = filedialog.askopenfile().name          
+        self.import_func(path)
         
-        return path
+    
+class EditMenu(ctk.CTkFrame):
+    def __init__(self, parent, close_edit):
+        super().__init__(parent)
+
+        # labels
+        self.label1 = ctk.CTkLabel(self, text='Text')
+        self.label2 = ctk.CTkLabel(self, text='Font')
+        self.label3 = ctk.CTkLabel(self, text='Color')
+        self.label4 = ctk.CTkLabel(self, text='Size')
+        self.label5 = ctk.CTkLabel(self, text='Opacity')
+        self.label6 = ctk.CTkLabel(self, text='Rotation')
+
+        # widgets
+        self.title = ctk.CTkLabel(self, text='Properties')
+        self.text = ctk.CTkEntry(self)
+        self.font = ctk.CTkComboBox(self)
+        self.color = ctk.CTkComboBox(self)
+        self.size = ctk.CTkSlider(self)
+        self.opacity = ctk.CTkSlider(self)
+        self.rotation = ctk.CTkSlider(self)
+        self.close = ctk.CTkButton(self, text='Remove Photo', command=close_edit)
+
+        # layout
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=4)
+        self.rowconfigure((0,1,2,3,4,5,6,7), weight=1)
+
+
+        self.grid(row=0, column=0, sticky='nsew')
+        self.title.grid(row=0, column=0, columnspan=2)
+
+        self.label1.grid(row=1, column=0)
+        self.text.grid(row=1, column=1)
+
+        self.label2.grid(row=2, column=0)
+        self.font.grid(row=2, column=1)
+
+        self.label3.grid(row=3, column=0)
+        self.color.grid(row=3, column=1)
+
+        self.label4.grid(row=4, column=0)
+        self.size.grid(row=4, column=1)
+
+        self.label5.grid(row=5, column=0)
+        self.opacity.grid(row=5, column=1)
+
+        self.label6.grid(row=6, column=0)
+        self.rotation.grid(row=6, column=1)
+
+        self.close.grid(row=7, column=0, columnspan=2)
+
+class ImageCanvas(Canvas):
+    def __init__(self, parent, resize_image):
+        super().__init__(parent, bg='black', bd=0, relief='ridge', highlightthickness=0)
+
+        # layout
+        self.grid(row=0, column=1, sticky='nsew')
+        self.bind('<Configure>', resize_image)
+
     
